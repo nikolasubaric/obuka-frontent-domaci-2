c13b95501d4b2269c1829c57669c17d8a239de61
nikolasubaric <nikolasubaric3@gmail.com>
2022-05-28 15:07:04 +0200

    Added movies

---
+++ app.js
@@ -0,0 +1,38 @@
+const movies = [
+  {
+    wasSeen: false,
+    name: "Dark Knights' Return VII: The Return of the Dark Knight",
+    year: 2004,
+    country: 'United States of America',
+    notice: 'Cointains graphic scenes, drug use, gambling',
+    actors: [
+      'Danny Devito',
+      'Mads Mikkelsen',
+      'Anne Hathaway',
+      'Aaron Paul',
+      'Jesse Plemons',
+    ],
+  },
+  {
+    wasSeen: false,
+    name: 'The Chipmunks 5: Escape from Alcatraz',
+    year: 2021,
+    country: 'United States of America',
+    notice: 'Contains nudity, drug use, gambling',
+    actors: [
+      'Anna Gunn',
+      'RJ Mitte',
+      'Bryan Cranston',
+      'Giancarlo Esposito',
+      'Dean Norris',
+    ],
+  },
+  {
+    wasSeen: false,
+    name: 'Truckmonster 5: Revved Up!',
+    year: 2004,
+    country: 'United States of America',
+    notice: 'Cointains graphic scenes, drug use, gambling',
+    actors: ['Owen Wilson', 'Paul Newman', 'George Carlin', 'Justin Long'],
+  },
+];
