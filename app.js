1f8f24eb6e79a9efaef5f8201d63aac7271dab48
nikolasubaric <nikolasubaric3@gmail.com>
2022-05-29 18:05:46 +0200

    finished task

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const output = document.getElementById('dataOutput');
-// console.log(output);
+
+const inputForm = document.getElementById('inputForm');
+const inputMovieName = document.getElementById('inputMovieName');
+const inputMovieYear = document.getElementById('inputMovieYear');
+const inputCountryName = document.getElementById('inputCountry');
+const inputWatched = document.getElementById('inputWatched');
+const inputActors = document.getElementById('inputActors');
+const inputNotice = document.getElementById('inputNotice');
+
+const myModal = new bootstrap.Modal('#exampleModal', {
+  keyboard: false,
+});
+
 const movies = [
   {
     wasSeen: false,
@@ -43,9 +55,9 @@ const movies = [
     ],
   },
 ];
-
-// VALIDACIJA FILMA PRIJE PRIKAZIVANJA
-
+///////////////////////////////////////
+// PRIKAZIVANJE FILMOVA IZ ARRAYA
+//////////////////////////////////////
 const populateTable = () => {
   let markup = ``;
 
@@ -54,7 +66,7 @@ const populateTable = () => {
            <tr class='${movie.wasSeen ? 'watched' : 'notWatched'}'>
             <td><input type='checkbox' ${
               movie.wasSeen && 'checked'
-            } data-movie='${movie.name}' /></td>
+            } data-movie='${movie.name}' class='checkbox' /></td>
             <td>${movie.name}</td>
             <td>${movie.year}</td>
             <td>${movie.country || ''}</td>
@@ -66,51 +78,117 @@ const populateTable = () => {
 
   output.innerHTML = markup;
 
-  const checkboxes = document.querySelectorAll('input[type=checkbox]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-      const parentRow = checkbox.closest('tr');
-      // console.log(parentRow);
-      if (checkbox.checked) {
-        parentRow.classList.remove('notWatched');
-        parentRow.classList.add('watched');
+  ///////////////////////////////////////
+  // DODAVANJE HANDLERA SVIM CHECKBOXOVIMA
+  //////////////////////////////////////
 
-        movies.forEach(movie => {
-          if (movie.name === checkbox.dataset.movie) {
-            movie.wasSeen = true;
-            console.log(movie);
-          }
-        });
-      } else {
-        parentRow.classList.remove('watched');
-        parentRow.classList.add('notWatched');
+  const checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach(checkbox => addCheckboxHandler(checkbox));
+};
 
-        movies.forEach(movie => {
-          if (movie.name === checkbox.dataset.movie) {
-            movie.wasSeen = false;
+function containsNumber(str) {
+  return /[0-9]/.test(str);
+}
 
-            console.log(movie);
-          }
-        });
-      }
-      // console.log(...movies);
-    });
+///////////////////////////////////////
+// VALIDACIJA SVIH INPUTA NA SUBMIT FORME
+//////////////////////////////////////
+
+inputForm.addEventListener('submit', event => {
+  event.preventDefault();
+  let ok = true;
+  let errorMessage = '\n';
+
+  if (containsNumber(inputCountryName.value)) {
+    errorMessage += 'Country names cannot contain numbers';
+    ok = false;
+  }
+
+  if (containsNumber(inputActors.value)) {
+    errorMessage += 'Actor names cannot contain numbers';
+    ok = false;
+  }
+
+  if (!ok) {
+    alert(errorMessage);
+    return;
+  }
+
+  const wasSeen = inputWatched.checked;
+  const name = inputMovieName.value;
+  const year = parseInt(inputMovieYear.value);
+  const notice = inputNotice.value;
+  const country = inputCountryName.value;
+  const actors = inputActors.value.split(',').map(actor => actor.trim());
+
+  const newMovie = { wasSeen, name, actors, country, notice, year };
+  movies.push(newMovie);
+
+  addMovie(newMovie);
+});
+
+///////////////////////////////////////
+// DODAVANJE FILMA U TABELU
+//////////////////////////////////////
+
+const addMovie = movie => {
+  let markup = `
+           <tr class='${movie.wasSeen ? 'watched' : 'notWatched'}'>
+            <td><input type='checkbox' ${
+              movie.wasSeen && 'checked'
+            } data-movie='${movie.name}' /></td>
+            <td>${movie.name}</td>
+            <td>${movie.year}</td>
+            <td>${movie.country || ''}</td>
+            <td>${movie.notice || ''}</td>
+            <td>${movie.actors.join(', ')}</td>
+          </tr>
+    `;
+  output.insertAdjacentHTML('beforeend', markup);
+  const checkbox = document.querySelector(`input[data-movie='${movie.name}']`);
+  addCheckboxHandler(checkbox);
+  clearModal();
+  myModal.toggle();
+};
+
+///////////////////////////////////////
+// METODA ZA DODAVANJE HANDLERA
+//////////////////////////////////////
+
+const addCheckboxHandler = checkbox => {
+  checkbox.addEventListener('change', () => {
+    const parentRow = checkbox.closest('tr');
+    if (checkbox.checked) {
+      parentRow.classList.remove('notWatched');
+      parentRow.classList.add('watched');
+
+      movies.forEach(movie => {
+        if (movie.name === checkbox.dataset.movie) {
+          movie.wasSeen = true;
+          console.log(movie);
+        }
+      });
+    } else {
+      parentRow.classList.remove('watched');
+      parentRow.classList.add('notWatched');
+
+      movies.forEach(movie => {
+        if (movie.name === checkbox.dataset.movie) {
+          movie.wasSeen = false;
+          console.log(movie);
+        }
+      });
+    }
   });
-  // console.log(checkboxes);
-  // console.log(checkboxes[0].checked);
+};
+
+const clearModal = () => {
+  inputActors.value = '';
+  inputCountryName.value = '';
+  inputMovieName.value = '';
+  inputMovieYear.value = null;
+  inputNotice.value = '';
+  inputWatched.checked = false;
 };
 
 populateTable();
-
-// const testMarkup = `
-// <tr class='${movies[0].wasSeen ? 'watched' : 'notWatched'}'>
-//  <td><input type='checkbox' ${movies[0].wasSeen && 'checked'} /></td>
-//  <td>${movies[0].name}</td>
-//  <td>${movies[0].year}</td>
-//  <td>${movies[0].country || ''}</td>
-//  <td>${movies[0].notice || ''}</td>
-//  <td>${movies[0].actors.join(', ')}</td>
-// </tr>
-// `;
-
-// output.insertAdjacentHTML('beforeend', testMarkup);
