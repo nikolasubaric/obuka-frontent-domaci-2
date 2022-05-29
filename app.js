3969e703197495129f172b9994ac47f3beb4cfc7
nikolasubaric <nikolasubaric3@gmail.com>
2022-05-29 13:39:00 +0200

    added checkbox functionality

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 const output = document.getElementById('dataOutput');
-console.log(output);
+// console.log(output);
 const movies = [
   {
     wasSeen: false,
-    name: "Dark Knights' Return VII: The Return of the Dark Knight",
-    year: 2004,
-    country: 'United States of America',
-    notice: 'Cointains graphic scenes, drug use, gambling',
+    name: 'The Northman',
+    year: 2022,
+    country: 'United States',
+    notice: 'R (Strong Bloody Violence|Some Sexual Content|Nudity)',
     actors: [
-      'Danny Devito',
-      'Mads Mikkelsen',
-      'Anne Hathaway',
-      'Aaron Paul',
-      'Jesse Plemons',
+      'Alexander Skarsgård',
+      'Nicole Kidman',
+      'Claes Bang',
+      'Ethan Hawke',
+      'Anya Taylor-Joy',
     ],
   },
   {
     wasSeen: false,
-    name: 'The Chipmunks 5: Escape from Alcatraz',
-    year: 2021,
-    country: 'Canary Islands',
-    notice: 'Contains nudity, drug use, gambling',
+    name: 'Top Gun',
+    year: 1986,
+    country: 'United States',
+    notice: 'PG',
     actors: [
-      'Anna Gunn',
-      'RJ Mitte',
-      'Bryan Cranston',
-      'Giancarlo Esposito',
-      'Dean Norris',
+      'Tom Cruise',
+      'Kelly McGillis',
+      'Anthony Edwards',
+      'Val Kilmer',
+      'Tom Skerritt',
     ],
   },
   {
     wasSeen: false,
-    name: 'Truckmonster 5: Revved Up!',
-    year: 2004,
-    country: 'Tunisia',
-    notice: 'Cointains graphic scenes, drug use, gambling',
-    actors: ['Owen Wilson', 'Paul Newman', 'George Carlin', 'Justin Long'],
+    name: 'Pirates of the Carribean: The Curse of the Black Pearl',
+    year: 2003,
+    country: 'United States',
+    notice: 'PG-13 (Adventure Violence|Action/Violence)',
+    actors: [
+      'Johnny Depp',
+      'Geoffrey Rush',
+      'Orlando Bloom',
+      'Keira Knightley',
+    ],
   },
 ];
 
@@ -47,7 +52,9 @@ const populateTable = () => {
   movies.forEach(movie => {
     markup += `
            <tr class='${movie.wasSeen ? 'watched' : 'notWatched'}'>
-            <td><input type='checkbox' ${movie.wasSeen && 'checked'} /></td>
+            <td><input type='checkbox' ${
+              movie.wasSeen && 'checked'
+            } data-movie='${movie.name}' /></td>
             <td>${movie.name}</td>
             <td>${movie.year}</td>
             <td>${movie.country || ''}</td>
@@ -58,6 +65,52 @@ const populateTable = () => {
   });
 
   output.innerHTML = markup;
+
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      const parentRow = checkbox.closest('tr');
+      // console.log(parentRow);
+      if (checkbox.checked) {
+        parentRow.classList.remove('notWatched');
+        parentRow.classList.add('watched');
+
+        movies.forEach(movie => {
+          if (movie.name === checkbox.dataset.movie) {
+            movie.wasSeen = true;
+            console.log(movie);
+          }
+        });
+      } else {
+        parentRow.classList.remove('watched');
+        parentRow.classList.add('notWatched');
+
+        movies.forEach(movie => {
+          if (movie.name === checkbox.dataset.movie) {
+            movie.wasSeen = false;
+
+            console.log(movie);
+          }
+        });
+      }
+      // console.log(...movies);
+    });
+  });
+  // console.log(checkboxes);
+  // console.log(checkboxes[0].checked);
 };
 
 populateTable();
+
+// const testMarkup = `
+// <tr class='${movies[0].wasSeen ? 'watched' : 'notWatched'}'>
+//  <td><input type='checkbox' ${movies[0].wasSeen && 'checked'} /></td>
+//  <td>${movies[0].name}</td>
+//  <td>${movies[0].year}</td>
+//  <td>${movies[0].country || ''}</td>
+//  <td>${movies[0].notice || ''}</td>
+//  <td>${movies[0].actors.join(', ')}</td>
+// </tr>
+// `;
+
+// output.insertAdjacentHTML('beforeend', testMarkup);
